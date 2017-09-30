7ecaa2effd5b7b9964cf2187300080946380044c
Emily Jones <efhjones@gmail.com>
2017-09-30 16:42:41 -0600

    add react

---
+++ src/js/Counter.js
@@ -0,0 +1,13 @@
+import React from 'react';
+ 
+const Counter = () => {
+  return (
+    <button
+      onClick={() => {
+        console.log("Hi!")
+      }}
+    >Oh hai
+    </button>
+  );
+};
+export default Counter;
